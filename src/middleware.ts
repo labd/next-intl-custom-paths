2ab925a8e409f9f2361ec95d5b5fcb94b9ea75a9
Michael van Tellingen <michaelvantellingen@gmail.com>
2023-12-21 22:51:40 +0100

    support localized paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,14 @@
 import createNextIntlMiddleware from "next-intl/middleware";
-import type { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 type Options = {
 	locales: string[];
 	defaultLocale: string;
-	pathLocaleMap: Record<string, string>
+	pathLocaleMap: Record<string, string>;
+	localizedPaths: Record<string, string>;
+	useLocaleForRoot: boolean;
 };
-
 
 export const createNextIntlCustomPathMiddleware = (options: Options) => {
 	const intlMiddleware = createNextIntlMiddleware({
@@ -18,26 +19,62 @@ export const createNextIntlCustomPathMiddleware = (options: Options) => {
 		localePrefix: "always",
 	});
 
-	const resolvePathLocale = (path: string): string | undefined =>
-	options.pathLocaleMap[path];
+	// Transform localizedPaths for faster lookups
+	const localizedPaths: Record<string, Record<string, string>> = {};
+	for (const [key, values] of Object.entries(options.localizedPaths)) {
+		for (const [locale, word] of Object.entries(values)) {
+			if (!localizedPaths[locale]) {
+				localizedPaths[locale] = {};
+			}
+			localizedPaths[locale][word] = key;
+		}
+	}
+
+	const resolvePathLocale = (path: string): string | undefined => {
+		if (options.pathLocaleMap[path]) {
+			return options.pathLocaleMap[path];
+		}
+	};
+
+	const resolveLocaleToPath = (locale: string): string | undefined =>
+		Object.keys(options.pathLocaleMap).find(
+			(key) => options.pathLocaleMap[key] === locale
+		);
 
 	return (request: NextRequest): NextResponse<unknown> => {
+		if (request.nextUrl.pathname === "/" && options.useLocaleForRoot) {
+			const path = resolveLocaleToPath(options.defaultLocale);
+			const url = new URL(`/${path}`, request.nextUrl.origin);
+			return NextResponse.redirect(url, 308);
+		}
+
 		const [, localePart, ...rest] = request.nextUrl.pathname.split("/");
 		const locale = resolvePathLocale(localePart);
+		const pathWithoutLocale = rest.join("/");
+
 		if (locale) {
-			const mappedURL = new URL(
-				`/${locale}/${rest.join("/")}`,
-				request.nextUrl.origin,
-			);
-			const newRequest = new NextRequest(mappedURL, request as Request);
-			return intlMiddleware(newRequest);
-		} else {
-			const mappedURL = new URL(
-				`/${options.defaultLocale}/${rest.join("/")}`,
-				request.nextUrl.origin,
-			);
+			const path = localizedPaths[locale]?.[pathWithoutLocale] ?? pathWithoutLocale;
+			const mappedURL = new URL(`/${locale}/${path}`, request.nextUrl.origin);
 			const newRequest = new NextRequest(mappedURL, request as Request);
 			return intlMiddleware(newRequest);
 		}
+
+		// If the full locale was used in the path, then we can redirect to the
+		// short locale path variant
+		if (resolveLocaleToPath(localePart)) {
+			const url = new URL(
+				`/${resolveLocaleToPath(localePart)}/${pathWithoutLocale}`,
+				request.nextUrl.origin
+			);
+			return NextResponse.redirect(url, 308);
+		}
+
+		// No locale was found in the path, so we can redirect to the default
+		const url = new URL(
+			`/${resolveLocaleToPath(options.defaultLocale)}/${pathWithoutLocale}`,
+			request.nextUrl.origin
+		);
+		const newRequest = new NextRequest(url, request as Request);
+		return intlMiddleware(newRequest);
 	};
 };
